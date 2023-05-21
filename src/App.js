f7a16132d6b7bac3fd78f60434f897923463a3c2
ss5815190 <ss5815190@gmail.com>
2023-05-22 00:47:33 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Exam from"./pages/exam"
 import Home from"./pages/home"
-
+import { HashRouter,BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      
-      <Exam/>
-    </div>
+    <HashRouter> 
+      <Routes>
+        <Route path="/" exact element={<Exam />} />
+        <Route path="/chactroom" element={<Home />} />
+      </Routes>
+    </HashRouter>
+    
   );
 }
 
