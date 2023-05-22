2b4c389444deb2a49d50748a00d7a7a65eb51639
ss5815190 <ss5815190@gmail.com>
2023-05-23 00:21:02 +0800

    Let users who are not login jump to the login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useContext } from "react";
 import Exam from"./pages/exam"
 import Home from"./pages/home"
-import { HashRouter,BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter,BrowserRouter, Route, Routes ,Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 function App() {
+  const {currentUser}=useContext(AuthContext)
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
+    }
+
+    return children
+  };
   return (
     <HashRouter> 
       <Routes>
         <Route path="/" exact element={<Exam />} />
-        <Route path="/chatroom" element={<Home />} />
+        <Route path="/chatroom" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+        } />
       </Routes>
     </HashRouter>
     
